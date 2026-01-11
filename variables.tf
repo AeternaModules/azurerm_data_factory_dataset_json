a867e960c9e778d99e0cf5e74b0aa76bce18b43f
Daniel Allen <dan@aeternagroup.net>
2026-01-11 01:42:50 +0000

    Update to schema version v4.57.0

---
+++ variables.tf
@@ -0,0 +1,66 @@
+variable "data_factory_dataset_jsons" {
+  description = <<EOT
+Map of data_factory_dataset_jsons, attributes below
+Required:
+    - data_factory_id
+    - linked_service_name
+    - name
+Optional:
+    - additional_properties
+    - annotations
+    - description
+    - encoding
+    - folder
+    - parameters
+    - azure_blob_storage_location (block):
+        - container (required)
+        - dynamic_container_enabled (optional)
+        - dynamic_filename_enabled (optional)
+        - dynamic_path_enabled (optional)
+        - filename (required)
+        - path (required)
+    - http_server_location (block):
+        - dynamic_filename_enabled (optional)
+        - dynamic_path_enabled (optional)
+        - filename (required)
+        - path (required)
+        - relative_url (required)
+    - schema_column (block):
+        - description (optional)
+        - name (required)
+        - type (optional)
+EOT
+
+  type = map(object({
+    data_factory_id       = string
+    linked_service_name   = string
+    name                  = string
+    additional_properties = optional(map(string))
+    annotations           = optional(list(string))
+    description           = optional(string)
+    encoding              = optional(string)
+    folder                = optional(string)
+    parameters            = optional(map(string))
+    azure_blob_storage_location = optional(object({
+      container                 = string
+      dynamic_container_enabled = optional(bool, false)
+      dynamic_filename_enabled  = optional(bool, false)
+      dynamic_path_enabled      = optional(bool, false)
+      filename                  = string
+      path                      = string
+    }))
+    http_server_location = optional(object({
+      dynamic_filename_enabled = optional(bool, false)
+      dynamic_path_enabled     = optional(bool, false)
+      filename                 = string
+      path                     = string
+      relative_url             = string
+    }))
+    schema_column = optional(object({
+      description = optional(string)
+      name        = string
+      type        = optional(string)
+    }))
+  }))
+}
+
